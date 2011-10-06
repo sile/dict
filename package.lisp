98a331fda1c0ae4a8e0441c5d5c739fe32d97e7f
Takeru Ohta <phjgt308@gmail.com>
2011-10-06 19:31:48 +0900

    32bit対応: 型定義を若干汎用化(SBCLのみ)

--- package.lisp
+++ package.lisp
@@ -20,21 +20,23 @@
            test-name))
 (in-package :dict)
 
-(deftype positive-fixnum () '(integer 0 #.most-positive-fixnum))
-(deftype array-index () '(unsigned-byte 29))
-(deftype hashcode () '(unsigned-byte 30))
-(deftype hashcode-width () '(mod 31))
-(deftype set-fn () '(function (t t dict) t))
-(deftype get-fn () '(function (t dict t) (values t boolean)))
-(deftype rem-fn () '(function (t dict) boolean))
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter *fastest* '(optimize (speed 3) (safety 0) (debug 0)))
   (defparameter *interface* '(optimize (speed 3) (safety 2) (debug 1)))
   (defparameter *normal* '(optimize (speed 1) (safety 3) (debug 2)))
   (defparameter *muffle-note* #-SBCL '(ignore)
                               #+SBCL '(sb-ext:muffle-conditions sb-ext:compiler-note))
-  (defconstant +HASHCODE_WIDTH+ 30)
-  (defconstant +MAX_HASHCODE+ #.(1- (ash 1 30))))
+  (defconstant +HASHCODE_WIDTH+ #+X86-64 30 #-X86-64 29))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defconstant +MAX_HASHCODE+ #.(1- (ash 1 +HASHCODE_WIDTH+))))
+
+(deftype positive-fixnum () '(integer 0 #.most-positive-fixnum))
+(deftype array-index () '(unsigned-byte #.+HASHCODE_WIDTH+))
+(deftype hashcode () '(unsigned-byte #.+HASHCODE_WIDTH+))
+(deftype hashcode-width () '(mod #.(1+ +HASHCODE_WIDTH+)))
+(deftype set-fn () '(function (t t dict) t))
+(deftype get-fn () '(function (t dict t) (values t boolean)))
+(deftype rem-fn () '(function (t dict) boolean))
 
 (defparameter *functor-repository* (make-hash-table :test #'eq))
